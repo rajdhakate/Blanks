bfb5fcdfd270a36e1fa065c5ac471d44d82a73b9
Raj Dhakate <rajdhakate@rajs-macbook-pro.local>
2018-02-03 00:26:58 +0530

    Basic Setup

---
+++ Blanks/StartViewController.swift
@@ -0,0 +1,52 @@
+//
+//  StartViewController.swift
+//  Blanks
+//
+//  Created by Raj Dhakate on 02/02/18.
+//  Copyright © 2018 Dhakate Codes. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var resumeButton: UIButton!
+    
+    private func setupView() {
+        startButton.setCornerRadius()
+        resumeButton.setCornerRadius()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupView()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension UIButton {
+    func setCornerRadius() {
+        self.layer.cornerRadius = 10
+        self.layer.borderWidth = 2
+        self.layer.borderColor = UIColor.lightGray.cgColor
+    }
+}
---
+++ Pods/Target Support Files/Pods-BlanksTests/Pods-BlanksTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BlanksTests : NSObject
+@end
+@implementation PodsDummy_Pods_BlanksTests
+@end
